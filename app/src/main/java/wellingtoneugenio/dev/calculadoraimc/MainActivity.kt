5a0da5d99381536b38e36ab9d8c51bc8c704b6f2
wellington <wellingtoneugenio@gmail.com>
2021-03-24 21:52:11 -0300

    testando lifecycle

--- app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
+++ app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
@@ -2,11 +2,42 @@ package wellingtoneugenio.dev.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle", "entrei no CREATE - estou criando a tela")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle", "entrei no RESUME - agora voc~e pode interagir com a tela")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifecycle", "PAUSE - a tela perdeu o foco. você não pode interagir")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.w("lifecycle","STOP - a tela não está visível mais ainda existe")
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        Log.w("lifecycle", "\nentrei no RESTART - a tela esta retomando o foco")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifecycle", "\nentrei no DESTROY -oh não! a tela foi destruída!")
     }
 }
