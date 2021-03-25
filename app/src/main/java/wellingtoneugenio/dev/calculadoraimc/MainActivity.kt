5bb74f3cd1c94f44b054612aa24bfab0e97f7f75
leto-silva <wellingtoneugenio@gmail.com>
2021-03-25 17:37:46 -0300

    Teste de linearlayouts, styles.xml

--- app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
+++ app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
@@ -12,34 +12,5 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.w("Lifecycle", "entrei no CREATE - estou criando a tela")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        Log.w("Lifecycle", "entrei no RESUME - agora voc~e pode interagir com a tela")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w("Lifecycle", "PAUSE - a tela perdeu o foco. você não pode interagir")
-    }
-
-    override fun onStop(){
-        super.onStop()
-        Log.w("Lifecycle","STOP - a tela não está visível mais ainda existe")
-        finish()
-    }
-
-    override fun onRestart(){
-        super.onRestart()
-        Log.w("Lifecycle", "\nentrei no RESTART - a tela esta retomando o foco")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("Lifecycle", "\nentrei no DESTROY -oh não! a tela foi destruída!")
-    }
 }
