eb55c2401c4aca29f5ffa7b25ac690a6ee91eed1
leto-silva <wellingtoneugenio@gmail.com>
2021-03-25 11:37:04 -0300

    Testes e reescrita dos métodos lifecycle

--- app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
+++ app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
@@ -9,35 +9,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     override fun onStart() {
         super.onStart()
-        Log.w("lifecycle", "entrei no CREATE - estou criando a tela")
+        Log.w("Lifecycle", "entrei no CREATE - estou criando a tela")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.w("lifecycle", "entrei no RESUME - agora voc~e pode interagir com a tela")
+        Log.w("Lifecycle", "entrei no RESUME - agora voc~e pode interagir com a tela")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.w("lifecycle", "PAUSE - a tela perdeu o foco. você não pode interagir")
+        Log.w("Lifecycle", "PAUSE - a tela perdeu o foco. você não pode interagir")
     }
 
     override fun onStop(){
         super.onStop()
-        Log.w("lifecycle","STOP - a tela não está visível mais ainda existe")
+        Log.w("Lifecycle","STOP - a tela não está visível mais ainda existe")
+        finish()
     }
 
     override fun onRestart(){
         super.onRestart()
-        Log.w("lifecycle", "\nentrei no RESTART - a tela esta retomando o foco")
+        Log.w("Lifecycle", "\nentrei no RESTART - a tela esta retomando o foco")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.w("lifecycle", "\nentrei no DESTROY -oh não! a tela foi destruída!")
+        Log.w("Lifecycle", "\nentrei no DESTROY -oh não! a tela foi destruída!")
     }
 }
