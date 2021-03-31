3f4f2cf3450fd65caf45ad78ad40570b8b54c250
wellington <wellingtoneugenio@gmail.com>
2021-03-31 01:28:47 -0300

    Colocando o codigo na aplicação

--- app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
+++ app/src/main/java/wellingtoneugenio/dev/calculadoraimc/MainActivity.kt
@@ -2,13 +2,25 @@ package wellingtoneugenio.dev.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListenes()
+
+    }
+
+    fun setListenes(){
+        alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
 
     }
 
